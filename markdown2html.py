b5ca3b2b6fbcc3fa9a27047ca0140d6fa6f6c003
amanuelgthn <amanuelgthn@gmail.com>
2023-12-29 10:39:55 +0100

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -7,15 +7,18 @@ Markdown2html.py
 from sys import argv, stderr
 from os import path
 
-args = argv
-if len(args) != 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=stderr)
-    exit(1)
-elif len(args) == 3:
-    if not path.isfile(args[1]):
-        print("Missing {}".format(args[1]), file=stderr)
+
+if __name__ == '__main__':
+    """Taking and checking arguments"""
+    args = argv
+    if len(args) != 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
-    elif not path.isfile(args[2]):
-        print("Missing {}".format(args[2]), file=stderr)
-        exit(1)
-    exit(0)
+    elif len(args) == 3:
+        if not path.isfile(args[1]):
+            print("Missing {}".format(args[1]), file=stderr)
+            exit(1)
+        elif not path.isfile(args[2]):
+            print("Missing {}".format(args[2]), file=stderr)
+            exit(1)
+        exit(0)
